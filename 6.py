9f8155b399d658cccf1b5f4eb576754f98cd561f
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-10-23 13:51:48 +0900

    Update 6.py

--- 6.py
+++ 6.py
@@ -27,3 +27,28 @@ if sum == n: 경우의 수 증가, end 증가, sum 갱신
 elif sum > n: sum 갱신, start증가
 else: end 증가, sum 갱신
 """
+
+# 특정한 합을 가지는 부분 연속 수열 찾기
+
+n = int(input())      # 찾고자 하는 부분합
+
+# 1 ~ n까지 원소를 가지는 배열 만들고 투포인터 알골리즘 수행
+data = [i for i in range(1, n + 1)]
+
+count = 0       # 부분합의 개수
+interval_sum = 0   # 부분합
+end = 0     # 끝점
+
+# start를 차례대로 증가시키며 반복
+for start in range(n):
+    # end를 가능한 만큼 이동시키기
+    while interval_sum < n and end < n:
+        interval_sum += data[end]
+        end += 1
+    # 부분합이 m일 때 카운트 증가
+    if interval_sum == n:
+        count += 1
+    # 부분합이 m보다 크거나 같으면 start를 1 증가시키기 때문에 이전 start값을 빼주어야 한다.
+    interval_sum -= data[start]
+
+print(count)
