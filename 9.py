e97e31c7987de0119c257218bb459da389796fa6
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-10-25 13:46:32 +0900

    Update 9.py

--- 9.py
+++ 9.py
@@ -5,18 +5,18 @@ s, p = map(int, input().split())
 data = input()
 
 a, c, g, t = map(int, input().split())
-start = data[:p]    # 시작 문자열
 
+# 시작문자열을 먼저 입력하고 조건에 해당하는지 먼저 확인(10 ~16)
+start = data[:p]    # 시작 문자열
 dic = {'A':0, 'C':0, 'G':0, 'T':0}
-# 11 ~ 16: start문자열이 부분 문자열 성립하는지 확인 하는 것
 for i in start:
     dic[i] += 1
 count = 0
 if dic['A'] >= a and dic['C'] >= c and dic['G'] >= g and dic['T'] >= t:
     count += 1
 
-s_index = 0
-e_index = s_index + p
+s_index = 0                 # 부분문자열의 시작 인덱스
+e_index = s_index + p       # 부분문자열의 끝 인덱스
 
 for i in range(s - p):
     # 1칸 오른쪽 으로 이동하면, 기존의 구간에 비해서 1개가 추가되고, 1개는 빠져 나간다
