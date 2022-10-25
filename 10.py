f0dd87e8a6f3906db53fa43bf8d33bd84b881719
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-10-25 14:10:32 +0900

    Update 10.py

--- 10.py
+++ 10.py
@@ -7,9 +7,9 @@ array = list(map(int, input().split()))
 q = deque()
 
 for i in range(n):
-    # 큐의 마지막 위치에서부터 현재 값보다 큰 값은 큐에서 제거
+    # 큐의 마지막 위치에서부터 들어오느값(현재 값)보다 큰 값은 큐에서 제거
     while q and q[-1][1] > array[i]:    # 들어갈 숫자보다 큰 것들은 전부 pop
-        q.pop()
+        q.pop()     # 큐의 마지막 원소들을 제거하므로 popleft (X)
 
     q.append((i, array[i]))     # 큐의 마지막 위치에 현재 값 저장((인덱스, 값))
 
