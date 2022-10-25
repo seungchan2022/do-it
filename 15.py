d1767747a56bf2dbc4dcf85ff5a32ef199a17b12
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-10-25 16:20:55 +0900

    Update 15.py

--- 15.py
+++ 15.py
@@ -4,6 +4,7 @@ for _ in range(n):
     array.append(int(input()))
 
 for i in range(n - 1):
+    # index가 n - i -1 인 이유는 정렬된 데이터는 다시 확인할 필요 X
     for j in range(0, n - 1 - i):
         if array[j] > array[j + 1]:
             array[j], array[j + 1] = array[j + 1], array[j]
