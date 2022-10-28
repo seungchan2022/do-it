d5c142cde43e03a2ca0d3da57e0d7d82c2b9c146
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-10-28 16:14:39 +0900

    Update 24.py

--- 24.py
+++ 24.py
@@ -9,23 +9,25 @@ import math
 
 n = int(input())
 
-# 소수 구하는 함수
+# 소수 판별 함수
 def prime(x):
-    # 2부터 x의 제곱급 까지만 확인
+    # 2부터 제곱근 까지만 확인
     for i in range(2, int(math.sqrt(x)) + 1):
         if x % i == 0:      # x가 해당 수로 나누어 떨어지면
             return False    # 소수 X
     return True
 
-def dfs(k):
-    if len(str(k)) == n:    # 해당 자릿수 이면
-        print(k)
-    else:   # n자리 까지 재귀함수 형태로 탐색
-        for i in range(1, 10):
-            if i % 2 == 0:  # 짝수이면
-                continue    # 더이상 탐색 불필요
-            if prime(k * 10 + i):    # 소수이면
-                dfs(k * 10 + i)
+def dfs(v):
+    if len(str(v)) == n:        # 해당 자릿수 이면
+        print(v)
+    else:       # n자리 까지 재귀함수 형태로 검색
+        # 뒤에 붙는 수 계산
+        for i in range(10):
+            if i % 2 == 0:      # 짝수라면
+                continue        # 더이상 탐색 불필요
+            # 뒤에 붙힌 새로운 수가 홀수 이면서 소수일때
+            if prime(v * 10 + i):   # 소수이면 자릿수 늘림(먼저 늘려서 확인해주고 맞으면 dfs 실행)
+                dfs(v * 10 + i)
 
 dfs(2)
 dfs(3)
