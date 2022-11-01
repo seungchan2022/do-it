da0f6d633ed8f5c50db6921e5f1c582b9d0f1d95
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-11-01 13:46:48 +0900

    Update 42.py

--- 42.py
+++ 42.py
@@ -12,6 +12,24 @@ for i in range(t):
     result = a * b / gcd(a, b)  # lcm(최소 공배수): a * b / gcd(최대 공약수)
     print(int(result))
 
+------------------------------------------------------------
+
+for tc in range(int(input())):
+    a, b = map(int, input().split())
+    
+    # 최대 공약수
+    def gcd(a, b):
+        while b > 0:
+            a, b = b, a%b
+        return a
+    
+    # 최소 공배수
+    def lcm(a, b):
+        result = a * b // gcd(a, b)
+        return result
+    print(lcm(a, b))
+    
+    
 """
 import math
 
