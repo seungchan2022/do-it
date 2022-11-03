0cf570428f9b421645f8c99a09aad20f7f116cff
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-11-03 17:26:48 +0900

    Update 46.py

--- 46.py
+++ 46.py
@@ -41,3 +41,44 @@ for i in result:
 최초에는 방문 도시가 1이고, 이동하지 않았으므로 방문 리스트에 0을 저장
 이후 방문하는 도시는 이전 도시의 방문 리스트 값 + 1을 방문 리스트에 저장하는 방식으로 이동 거리 저장
 """
+
+-------------------------------------------------------------------------------
+
+from collections import deque
+
+n, m, k, x = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+distance = [-1] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+def bfs(x):
+    q = deque()
+    q.append(x)
+    distance[x] = 0
+
+    while q:
+        v = q.popleft()
+        for i in graph[v]:
+            if distance[i] == -1:   # 아직 방문하지 않았다면
+                distance[i] = distance[v] + 1
+                q.append(i)
+
+bfs(x)
+
+result = []
+
+for i in range(1, n + 1):
+    if distance[i] == k:
+        result.append(i)
+
+result.sort()
+
+if len(result) == 0:
+    print(-1)
+else:
+    for i in result:
+        print(i)
