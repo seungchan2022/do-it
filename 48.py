ffd79b4562597e4cda0b079357aac8e030233723
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-11-03 19:16:34 +0900

    Update 48.py

--- 48.py
+++ 48.py
@@ -7,23 +7,25 @@ input = sys.stdin.readline
 def bfs(start):
     q = deque()
     q.append(start)
-    visited[start] = 1 # 시작점 그룹은 1로
+    visited[start] = 1  # 시작점 그룹을 1로 설정
 
     while q:
-        v = q.popleft() # 현재 정점
-        for i in graph[v]: # 현재 정점과 연결된 정점이
-            if visited[i] == 0: # 방문하지 않았다면 현재 정점과 다른 그룹으로 지정
+        v = q.popleft()
+        for i in graph[v]:
+            if visited[i] == 0: # 방문하지 않은 정점이면
+                visited[i] = -visited[v]    # 현재 정점과 다른 그룹으로 지정
                 q.append(i)
-                visited[i] = -visited[v]
-            elif visited[i] == visited[v]: # 이미 방문한 정점이 같은 그룹이라면 False
+            elif visited[i] == visited[v]:  # 이미 방문한 정점이 같은 그룹이라면 False
                 return False
     return True
 
+
 for tc in range(int(input())):
     v, e = map(int, input().split())
-    graph = [[] for _ in range(v+1)] # 연결 리스트
-    visited = [0] * (v + 1) # 방문한 정점 체크
-    result = True # 이분그래프 여부
+    
+    graph = [[] for _ in range(v + 1)]
+    visited = [0] * (v + 1)     # 방문한 정점 체크
+    result = True       # 이분 그래프 여부 변수
 
     for _ in range(e):
         a, b = map(int, input().split())
@@ -31,8 +33,8 @@ for tc in range(int(input())):
         graph[b].append(a)
 
     for i in range(1, v + 1):
-        if visited[i] == 0: # 방문하지 않은 정점이면 bfs 수행
-            if not bfs(i): # 이분 그래프가 아니라면 탐색 중단
+        if visited[i] == 0:     # 방문하지 않은 정점이면 bfs 수행
+            if not bfs(i):  # 이분 그래프가 아니라면 탐색 중단
                 result = False
                 break
     
@@ -40,6 +42,7 @@ for tc in range(int(input())):
         print('YES')
     else:
         print('NO')
+        
 
 """
 1. 필요한 정보를 입력받은 뒤 방문한 정점을 체크할 수 있는 visited와 이분그래프인지 확인할 수 있는 변수 result를 생성한다.
