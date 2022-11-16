cab604f3a544052525f3c03ce87bece89f46391c
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-11-16 16:01:14 +0900

    Update 90.py

--- 90.py
+++ 90.py
@@ -1,14 +1,12 @@
 # 9252
 
 import sys
-input = sys.stdin.readline
 sys.setrecursionlimit(1000)
 
-a = input().rstrip()
-b = input().rstrip()
+a = input()
+b = input()
 
 dp = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
-data = []   # LCS(문자열) 저장 리스트
 
 for i in range(1, len(a) + 1):
     for j in range(1, len(b) + 1):
@@ -20,23 +18,25 @@ for i in range(1, len(a) + 1):
 
 print(dp[len(a)][len(b)])
 
+result = []
 # LCS 구현 함수
 def lcs(r, c):
     if r == 0 or c == 0:    # LCS 길이가 0이면
-        return      # 출력 안함
+        return
     # 맨 마지막 부터 탐색 하면서 문자열이 같으면 해당 문자 저장후 왼쪽 위로 이동
     if a[r - 1] == b[c - 1]:
-        data.append(a[r - 1])
+        result.append(a[r - 1])
         lcs(r - 1, c - 1)
-    # 다르면 max(왼, 위)로 이동
+    # 다르면 max(왼, 위)이동
     else:
-        if dp[r - 1][c] > dp[r][c - 1]:     # 위쪽
-            lcs(r - 1, c)
-        else:   # 왼쪽
+        if dp[r - 1][c] > dp[r][c - 1]:     # 위
+            lcs(r - 1, c)   
+        else:   # 왼
             lcs(r, c - 1)
 
 # 문자열 생성
 lcs(len(a), len(b))
 
-for i in range(len(data) - 1, -1 , -1):
-    print(data[i], end='')
+result.reverse()
+for i in result:
+    print(i, end='')
