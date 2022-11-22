9b2ab63a64a580b0d71436e3ee83ef53d56b0a89
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-11-22 15:48:54 +0900

    Update 96.py

--- 96.py
+++ 96.py
@@ -49,3 +49,8 @@ for i in range(N, 0, -1):
 
 for i in range(1, maxLength + 1):
     print(ans[i], end=' ')
+    
+
+    
+# https://ggam-nyang.tistory.com/40
+# https://seohyun0120.tistory.com/entry/%EA%B0%80%EC%9E%A5-%EA%B8%B4-%EC%A6%9D%EA%B0%80%ED%95%98%EB%8A%94-%EB%B6%80%EB%B6%84-%EC%88%98%EC%97%B4LIS-%EC%99%84%EC%A0%84-%EC%A0%95%EB%B3%B5-%EB%B0%B1%EC%A4%80-%ED%8C%8C%EC%9D%B4%EC%8D%AC
