c898276f7975eb14d61dc123a5f0b676675a852e
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-11-22 12:46:40 +0900

    Update 93.py

--- 93.py
+++ 93.py
@@ -56,3 +56,49 @@ mp[i][j]: i -> j로 이동하는데 드는 힘
 # 오른발 이동      현재 오른발 위치: i + i -> R 위치로 이동
     dp[N][L][R] = mind(dp[N - 1][L][i] + mp[i][R], dp[N][L][R])
 """
+
+----------------------------------------------------------------------------------------------
+
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+array = list(map(int, input().split()))
+
+d = [[[INF for k in range(5)] for j in range(5)] for i in range(100001)]
+
+# mp[i][j]: i -> j로 이동했을떄 드는 힘
+mp = [[0, 2, 2, 2, 2], [2, 1, 3, 4, 3], [2, 3, 1, 3, 4], [2, 4, 3, 1, 3], [2, 3, 4, 3, 1]]
+
+s = 1   # 수열 수행 횟수
+index = 0   # array의 index
+
+d[0][0][0] = 0
+
+while array[index] != 0:
+    n = array[index]    # 수열 값(발이 위치해야 할 곳)
+    
+    for i in range(5):
+        if n == i:  # 두 발이 같은 곳 X
+            continue
+        for j in range(5):
+            # 오른발 옮기기 j -> n
+            d[s][i][n] = min(d[s - 1][i][j] + mp[j][n], d[s][i][n])
+
+    for j in range(5):
+        if n == j:  # 두 발이 같은 곳 X
+            continue
+        for i in range(5):
+            # 왼발 옮기기 i -> n
+            d[s][n][j] = min(d[s - 1][i][j] + mp[i][n], d[s][n][j])
+
+    s += 1
+    index += 1
+
+result = INF
+
+for i in range(5):
+    for j in range(5):
+        result = min(result, d[len(array) - 1][i][j])
+
+print(result)
